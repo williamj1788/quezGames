383eecaf552b40800243f526ddfe4856db90498f
Jacquez Williams <jacquezwilliams115@gmail.com>
2019-02-08 17:44:58 -0500

    Added background and titles on home page

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './HomeStyle.css';
+
+export default class Home extends React.Component{
+    render(){
+        return(
+            <div id="main">
+                <div id="container">
+                    <h1 className="title">QuezGames</h1>
+                    <h3 className="title-sub">Click To Play A Game</h3>
+                    <div className="gameContainer"></div>
+                </div>
+            </div>
+        )
+    }
+}
