ecdb4389c324e3b80b9f310c9ccdf59000817576
Jacquez Williams <jacquezwilliams115@gmail.com>
2019-02-09 06:22:52 -0500

    Completed card background

--- src/TTTCard.js
+++ src/TTTCard.js
@@ -3,8 +3,9 @@ import React from 'react';
 export default class TTTCard extends React.Component{
     render(){
         return(
-            <div id="TTT-card">
-                <h3 class="card-title">Tick Tack Toe</h3>
+            <div className="TTT card">
+                <h3 className="card-title">Tick Tack Toe</h3>
+                <h3 className="card-description">Play A Classic Game of Tick Tack Toe</h3>
             </div>
         )
     }
