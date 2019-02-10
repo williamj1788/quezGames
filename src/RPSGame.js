41a6409b38a32c2eca0c311163091f1a7a7554d4
Jacquez Williams <jacquezwilliams115@gmail.com>
2019-02-10 16:51:49 -0500

    updated player card

--- src/RPSGame.js
+++ src/RPSGame.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import RPSPlayer from './RPSPlayer';
 
 export default class RPSGame extends React.Component{
     render(){
         return(
             <div id="RPS-container">
-                <div>Hello World</div>
+                <RPSPlayer />
                 <div>Hello World</div>
                 <div>Hello World</div>
             </div>
--- src/RPSPlayer.js
+++ src/RPSPlayer.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
+var rock = require("./Img/Rock.png");
+var paper = require("./Img/Paper.png");
+var scissors = require("./Img/sci.png");
+
 export default class RPSPlayer extends React.Component{
     render(){
         return(
-            <div>
-            
+            <div id="RPS-player">
+                <div className="score-div">
+                    <span className="RPS-player-score">Score: </span>
+                    <span className="RPS-player-score">Score</span>
+                </div>
+                <img className="RPS-imgs" src= {rock} />
+                <p className="RPS-player-score" style={{textAlign: "center"}} >Draw</p>
             </div>
         )
     }
