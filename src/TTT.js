9c04d95ffbfab5d8624f3de2ae7788e8c59a906e
Jacquez Williams <jacquezwilliams115@gmail.com>
2019-02-10 13:31:26 -0500

    made TTT responsive

--- src/TTT.js
+++ src/TTT.js
@@ -46,7 +46,7 @@ export default class TTT extends React.Component{
                         <h1 className="title game">Tick Tack Toe</h1>
                         <button className="homeButton" type="button" onClick={this.handleOnClick}>Home</button>
                     </div>
-                    <div style={{width: "400px", margin: "0 auto", paddingTop: "50px"}}>
+                    <div id="score-container">
                         <div style={{display: "flex", justifyContent: "space-between"}}>
                             <p className="computer">Computer: </p>
                             <p className="computer">{this.state.computerScore}</p>
