47d679231603afafcace0e54817d669b5c6420f5
Jacquez Williams <jacquezwilliams115@gmail.com>
2019-02-11 03:40:01 -0500

    added state to RPSGame and functions

--- src/RPSGame.js
+++ src/RPSGame.js
@@ -2,12 +2,53 @@ import React from 'react';
 import RPSPlayer from './RPSPlayer';
 
 export default class RPSGame extends React.Component{
+    constructor(props){
+        super(props);
+        this.state ={
+            player: {
+                score: 0,
+                play: "none"
+            },
+            computer: {
+                score: 0,
+                play: "none"
+            }
+        }
+        this.choosePlayForPlayer = this.choosePlayForPlayer.bind(this);
+        this.choosePlayForComputer = this.choosePlayForComputer.bind(this);
+    }
+
+    choosePlayForPlayer(play){
+        this.setState({
+            player: {
+                score: this.state.player.score,
+                play: play,
+            }
+        });
+    }
+
+    choosePlayForComputer(play){
+        this.setState({
+            computer: {
+                score: this.state.computer.score,
+                play: play,
+            }
+        });
+    }
+    
     render(){
         return(
-            <div id="RPS-container">
-                <RPSPlayer />
-                <div>Hello World</div>
-                <div>Hello World</div>
+            <div>
+                <div id="RPS-container">
+                    <RPSPlayer color={"#00ADF9"} player={"Player"} score={this.state.player.score} play={this.state.player.play} />
+                    <div ><p id="RPS-winner">Winner</p></div>
+                    <RPSPlayer color={"#FF0000"} player={"Computer"} score={this.state.computer.score} play={this.state.player.play} />
+                </div>
+                <div id="RPS-button-container">
+                    <button className="RPS-button">Rock</button>
+                    <button className="RPS-button">Paper</button>
+                    <button className="RPS-button">Scissors</button>
+                </div>
             </div>
         )
     }
--- src/RPSPlayer.js
+++ src/RPSPlayer.js
@@ -5,15 +5,17 @@ var paper = require("./Img/Paper.png");
 var scissors = require("./Img/sci.png");
 
 export default class RPSPlayer extends React.Component{
+    
+    
     render(){
         return(
             <div id="RPS-player">
                 <div className="score-div">
-                    <span className="RPS-player-score">Score: </span>
-                    <span className="RPS-player-score">Score</span>
+                    <span className="RPS-player-score" style={{color: this.props.color}}>{this.props.player + ":"} </span>
+                    <span className="RPS-player-score" style={{color: this.props.color}}>{this.props.score}</span>
                 </div>
                 <img className="RPS-imgs" src= {rock} />
-                <p className="RPS-player-score" style={{textAlign: "center"}} >Draw</p>
+                <p className="RPS-player-score" style={{color: this.props.color, textAlign: "center"}}>{this.props.play}</p>
             </div>
         )
     }
