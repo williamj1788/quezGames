818cd133f159ad944a7567634e365114a26bc56d
Jacquez Williams <jacquezwilliams115@gmail.com>
2019-02-12 17:30:26 -0500

    Made the Hint button work and the guesschar renders damically

--- src/GGGame.js
+++ src/GGGame.js
@@ -3,6 +3,12 @@ import GGGuessWord from './GGGuessWord';
 import GGGuessedWord from './GGGuessedWord';
 import GGInput from './GGInput';
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
 export default class GGGame extends React.Component{
     constructor(props){
         super(props);
@@ -11,8 +17,10 @@ export default class GGGame extends React.Component{
             GWords: [],
             hints: 4,
             Guesses: 10,
+            wordIndex: [],
         }
         this.addToGWords = this.addToGWords.bind(this);
+        this.getWordIndex = this.getWordIndex.bind(this);
     }
 
     addToGWords(word){
@@ -24,17 +32,33 @@ export default class GGGame extends React.Component{
                 Guesses: this.state.Guesses - 1, 
             }); 
         }
-        
     }
     
+    getWordIndex(){
+        if(this.state.hints > 0){
+            let length = this.state.word.length;
+            let wordI = this.state.wordIndex;
+            let number = getRandomInt(0, length);
+            if(wordI.indexOf(number) === -1){
+                wordI.push(number);
+                this.setState({
+                    wordIndex: wordI,
+                    hints: this.state.hints - 1,
+                });
+            }else{
+                this.getWordIndex();
+            } 
+        }
+        
+    }
     
     render(){
         return(
             <div>
                 <h2 className="GGGame-title">I'm Thinking Of A Word</h2>
-                <GGGuessWord word={this.state.word}/>
+                <GGGuessWord word={[this.state.word,this.state.wordIndex]}/>
                 <GGGuessedWord Words={this.state.GWords}/>
-                <GGInput onEnter={this.addToGWords} Guesses={this.state.Guesses} />
+                <GGInput onEnter={this.addToGWords} Guesses={this.state.Guesses} Hints={this.state.hints} onClick={this.getWordIndex} />
             </div>
         )
     }
--- src/GGInput.js
+++ src/GGInput.js
@@ -4,6 +4,7 @@ export default class GGInput extends React.Component{
     constructor(props){
         super(props);
         this.handleOnEnter = this.handleOnEnter.bind(this);
+        this.handleOnClick = this.handleOnClick.bind(this);
     }
 
     handleOnEnter(e){
@@ -11,11 +12,14 @@ export default class GGInput extends React.Component{
             this.props.onEnter(e.target.value);
         }
     }
+    handleOnClick(){
+        this.props.onClick();
+    }
     
     render(){
         return(
             <div className="GGGame-input-Container">
-                <button className="GGGame-button Hint">Hint: <span>0</span></button>
+                <button className="GGGame-button Hint" onClick={this.handleOnClick}>Hint: <span>{this.props.Hints}</span></button>
                 <input className="GGGame-input" type='text' placeholder="Guess the word" onKeyUp={this.handleOnEnter} />
                 <button className="GGGame-button Guess">Guess: <span>{this.props.Guesses}</span></button>
             </div>
