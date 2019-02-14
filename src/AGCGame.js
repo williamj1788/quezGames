6129f7687b74588f521f7f4898eaa70d3e67bd6a
Jacquez Williams <jacquezwilliams115@gmail.com>
2019-02-13 21:57:59 -0500

    finished the adventure template

--- src/AGCGame.js
+++ src/AGCGame.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import AGCLog from './AGCLog';
+import AGCInput from './AGCInput';
 
 export default class AGCGame extends React.Component{
     render(){
         return(
             <div>
                 <AGCLog />
+                <AGCInput />
             </div>
         )
     }
---
+++ src/AGCInput.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class AGCInput extends React.Component{
+    render(){
+        return(
+            <div className="AGC-button-container">
+                <button className="AGC-button">Hello</button>
+                <button className="AGC-button">Hello</button>
+                <button className="AGC-button">Hello</button>
+            </div>
+        )
+    }
+}
