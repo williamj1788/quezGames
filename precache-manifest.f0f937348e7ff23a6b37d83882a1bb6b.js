32094851505ca3b808c13e4196f113ad6fce1697
williamj1788 <jacquezwilliams115@gmail.com>
2019-11-13 09:48:40 -0500

    Updates

---
+++ precache-manifest.f0f937348e7ff23a6b37d83882a1bb6b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "11a067f29183d1e48376cd761683f500",
+    "url": "/static/media/BlueCircle.11a067f2.png"
+  },
+  {
+    "revision": "b39de682eacbae1d8294",
+    "url": "/static/css/main.bb7b1772.chunk.css"
+  },
+  {
+    "revision": "ee08a363b5100ed9c9a2",
+    "url": "/static/js/1.ee08a363.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5e6b6fb4daebb779b29ebf39b915d9dd",
+    "url": "/static/media/x.5e6b6fb4.png"
+  },
+  {
+    "revision": "b39de682eacbae1d8294",
+    "url": "/static/js/main.b39de682.chunk.js"
+  },
+  {
+    "revision": "4e7360ce6cd16243875d767175d63f5c",
+    "url": "/static/media/White.4e7360ce.jpg"
+  },
+  {
+    "revision": "0e73a56f8ace973a2bf26f9e4878866a",
+    "url": "/static/media/tic_tac_toe.0e73a56f.gif"
+  },
+  {
+    "revision": "8eb66576a57eda8470947d40468d3a60",
+    "url": "/static/media/rock,_paper_larger.8eb66576.jpg"
+  },
+  {
+    "revision": "a72cefffd1f43cca5232e4fa13393c93",
+    "url": "/static/media/boombox_background.a72cefff.jpg"
+  },
+  {
+    "revision": "52475d364ed0416f070dd93fdf7d7900",
+    "url": "/index.html"
+  }
+];
