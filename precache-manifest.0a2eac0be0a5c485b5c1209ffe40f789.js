9ed19242e074d2a619bc7ba0c0b5d29d4e7e4ed2
williamj1788 <jacquezwilliams115@gmail.com>
2019-12-09 11:41:30 -0500

    Updates

---
+++ precache-manifest.0a2eac0be0a5c485b5c1209ffe40f789.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86926d8d187c88518329d9df0ceb77b8",
+    "url": "/quezGames/index.html"
+  },
+  {
+    "revision": "1a47062a3bf7de1721b5",
+    "url": "/quezGames/static/css/main.6bf0c585.chunk.css"
+  },
+  {
+    "revision": "96c9a484dc3d684d649a",
+    "url": "/quezGames/static/js/2.282b6f1e.chunk.js"
+  },
+  {
+    "revision": "1a47062a3bf7de1721b5",
+    "url": "/quezGames/static/js/main.ee56ce0d.chunk.js"
+  },
+  {
+    "revision": "5d4e9eaa7463365c31af",
+    "url": "/quezGames/static/js/runtime-main.67788627.js"
+  },
+  {
+    "revision": "11a067f29183d1e48376cd761683f500",
+    "url": "/quezGames/static/media/BlueCircle.11a067f2.png"
+  },
+  {
+    "revision": "4e7360ce6cd16243875d767175d63f5c",
+    "url": "/quezGames/static/media/White.4e7360ce.jpg"
+  },
+  {
+    "revision": "a72cefffd1f43cca5232e4fa13393c93",
+    "url": "/quezGames/static/media/boombox_background.a72cefff.jpg"
+  },
+  {
+    "revision": "8eb66576a57eda8470947d40468d3a60",
+    "url": "/quezGames/static/media/rock,_paper_larger.8eb66576.jpg"
+  },
+  {
+    "revision": "0e73a56f8ace973a2bf26f9e4878866a",
+    "url": "/quezGames/static/media/tic_tac_toe.0e73a56f.gif"
+  },
+  {
+    "revision": "5e6b6fb4daebb779b29ebf39b915d9dd",
+    "url": "/quezGames/static/media/x.5e6b6fb4.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/quezGames/precache-manifest.b66a8c62b3ab5371b375392bf2c06fc2.js"
+  "/quezGames/precache-manifest.0a2eac0be0a5c485b5c1209ffe40f789.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/quezGames/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/quezGames/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
