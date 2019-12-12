02316636639320e9ff4cba313dc3ed08fa41220c
williamj1788 <jacquezwilliams115@gmail.com>
2019-12-12 15:40:44 -0500

    refactor: GGInput component is now a functional component

--- src/Components/Guessing-Game/GGInput.jsx
+++ src/Components/Guessing-Game/GGInput.jsx
@@ -1,38 +1,29 @@
 import React from "react";
 
-export default class GGInput extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleOnEnter = this.handleOnEnter.bind(this);
-    this.handleOnClick = this.handleOnClick.bind(this);
-  }
-
-  handleOnEnter(e) {
+function GGInput({ onEnter, onClick, Guesses, Hints }) {
+  function handleOnEnter(e) {
     if (e.keyCode === 13) {
-      this.props.onEnter(e.target.value);
+      onEnter(e.target.value);
       e.target.value = "";
     }
   }
-  handleOnClick() {
-    this.props.onClick();
-  }
 
-  render() {
-    return (
-      <div className="GGGame-input-Container">
-        <button className="GGGame-button Hint" onClick={this.handleOnClick}>
-          Hint: <span>{this.props.Hints}</span>
-        </button>
-        <input
-          className="GGGame-input"
-          type="text"
-          placeholder="Guess the word"
-          onKeyUp={this.handleOnEnter}
-        />
-        <button className="GGGame-button Guess">
-          Guess: <span>{this.props.Guesses}</span>
-        </button>
-      </div>
-    );
-  }
+  return (
+    <div className="GGGame-input-Container">
+      <button className="GGGame-button Hint" onClick={onClick}>
+        Hint: <span>{Hints}</span>
+      </button>
+      <input
+        className="GGGame-input"
+        type="text"
+        placeholder="Guess the word"
+        onKeyUp={handleOnEnter}
+      />
+      <button className="GGGame-button Guess">
+        Guess: <span>{Guesses}</span>
+      </button>
+    </div>
+  );
 }
+
+export default GGInput;
