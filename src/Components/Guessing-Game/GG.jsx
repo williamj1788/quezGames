eaeaee5dcf63f070b1865971200e3d17b126ab90
williamj1788 <jacquezwilliams115@gmail.com>
2019-12-12 14:38:39 -0500

    refactor: GG component is now a functional component

--- src/Components/Guessing-Game/GG.jsx
+++ src/Components/Guessing-Game/GG.jsx
@@ -1,32 +1,20 @@
 import React from "react";
 import GGGame from "./GGGame";
 
-export default class GG extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleOnClick = this.handleOnClick.bind(this);
-  }
-  handleOnClick() {
-    this.props.onClick();
-  }
-
-  render() {
-    return (
-      <div id="main">
-        <div id="container">
-          <div className="titleBar">
-            <h1 className="title game">Guessing Game</h1>
-            <button
-              className="homeButton"
-              type="button"
-              onClick={this.handleOnClick}
-            >
-              Home
-            </button>
-          </div>
-          <GGGame />
+function GG({ onClick }) {
+  return (
+    <div id="main">
+      <div id="container">
+        <div className="titleBar">
+          <h1 className="title game">Guessing Game</h1>
+          <button className="homeButton" type="button" onClick={onClick}>
+            Home
+          </button>
         </div>
+        <GGGame />
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+export default GG;
