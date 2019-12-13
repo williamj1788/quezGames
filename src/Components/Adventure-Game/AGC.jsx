9318da7869a4b7918d9dc26e77c167656c8de323
williamj1788 <jacquezwilliams115@gmail.com>
2019-12-13 15:29:42 -0500

    refactor: AGC component is now a functional component

--- src/Components/Adventure-Game/AGC.jsx
+++ src/Components/Adventure-Game/AGC.jsx
@@ -1,31 +1,20 @@
 import React from "react";
 import AGCGame from "./AGCGame";
 
-export default class AGC extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleOnClick = this.handleOnClick.bind(this);
-  }
-  handleOnClick() {
-    this.props.onClick();
-  }
-  render() {
-    return (
-      <div id="main">
-        <div id="container">
-          <div className="titleBar">
-            <h1 className="title game">Adventure Game</h1>
-            <button
-              className="homeButton"
-              type="button"
-              onClick={this.handleOnClick}
-            >
-              Home
-            </button>
-          </div>
-          <AGCGame />
+function AGC({ onClick }) {
+  return (
+    <div id="main">
+      <div id="container">
+        <div className="titleBar">
+          <h1 className="title game">Adventure Game</h1>
+          <button className="homeButton" type="button" onClick={onClick}>
+            Home
+          </button>
         </div>
+        <AGCGame />
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+export default AGC;
