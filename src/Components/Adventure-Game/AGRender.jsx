4e6bf3ee14c3218ed0240d5e2114e8d6af37885b
williamj1788 <jacquezwilliams115@gmail.com>
2019-12-13 20:37:23 -0500

    refactor: AGRender component is now a functional component

--- src/Components/Adventure-Game/AGRender.jsx
+++ src/Components/Adventure-Game/AGRender.jsx
@@ -1,59 +1,36 @@
 import React from "react";
 
-export default class AGRender extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handeOnClick = this.handeOnClick.bind(this);
-  }
-
-  handeOnClick(option) {
-    this.props.onClick(option);
-  }
-
-  render() {
-    if (this.props.loading) {
-      return (
-        <div>
-          <textarea className="AGC-log" value={"loading"} readOnly></textarea>
-        </div>
-      );
-    }
-    let content = "space-around";
-    let options = "null";
-    if (this.props.node !== "") {
-      options = this.props.node.options;
-      if (options) {
-        options = options.map((item, index) => {
-          return (
-            <button
-              className="AGC-button"
-              onClick={() => {
-                this.handeOnClick(item);
-              }}
-              key={"button" + index}
-            >
-              {item.text}
-            </button>
-          );
-        });
-      }
-      content = options.length < 3 ? "space-around" : "space-between";
-    }
-
+function AGRender({ onClick, loading, node, text }) {
+  if (loading) {
     return (
       <div>
-        <textarea
-          className="AGC-log"
-          value={this.props.text}
-          readOnly
-        ></textarea>
-        <div
-          className="AGC-button-container"
-          style={{ justifyContent: content }}
-        >
-          {options}
-        </div>
+        <textarea className="AGC-log" value="loading" readOnly />
       </div>
     );
   }
+
+  return (
+    <div>
+      <textarea className="AGC-log" value={text} readOnly></textarea>
+      <div
+        className="AGC-button-container"
+        style={{
+          justifyContent:
+            node.options.length < 3 ? "space-around" : "space-between"
+        }}
+      >
+        {node.options.map((item, index) => (
+          <button
+            className="AGC-button"
+            onClick={() => onClick(item)}
+            key={index}
+          >
+            {item.text}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 }
+
+export default AGRender;
